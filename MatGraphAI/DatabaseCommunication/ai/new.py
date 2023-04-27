66a7ab78c0f708e5aac9555e89858847df679377
mdreger <max.dreger92@gmail.com>
2023-04-27 15:13:35 +0200

    OntologiesAdded

--- MatGraphAI/DatabaseCommunication/ai/new.py
+++ MatGraphAI/DatabaseCommunication/ai/new.py
@@ -28,12 +28,15 @@ def chat_with_gpt3_5(setup_message=[], prompt='', api_key=''):
 
 def create_new_class(create_class, s_class, target_onto):
     y = create_class
+    s_class = tuple(s_class)
 
     with target_onto:
-        if s_class == 'Thing':
-            new_class = types.new_class(y, (Thing,))
-        else:
-            new_class = types.new_class(y, (onto[s_class],))
+            class alternative_labels(AnnotationProperty):
+                pass
+            class name(AnnotationProperty):
+                pass
+
+            new_class = types.new_class(y, s_class)
 
     return new_class
 
@@ -42,20 +45,23 @@ def copy_class_recursive(source_class, target_ontology, source_ontology, api_key
     target_class = target_ontology[source_class.name]
 
     if target_class is None:
+        print("hioer")
         # Create the new class in the target ontology
-        target_class = create_new_class(source_class.name, 'Thing', target_ontology)
+        parent_classes = source_class.is_a
+        target_class = create_new_class(source_class.name, parent_classes, target_ontology)
         # Call the custom function and store the output as an attribute of the class
         try:
             custom_function_output = json.loads(custom_function(ONTOLOGY_ASSISTANT_MESSAGES, source_class.name, api_key))
             target_class.comment = custom_function_output["description"]
-            target_class.label = custom_function_output["alternative_labels"]
+            target_class.alternative_labels = str(custom_function_output["alternative_labels"])
+            target_class.name = source_class.name
         except:
             print("Fehler")
     # Copy and create subclasses recursively
     for subclass in source_class.subclasses():
-        target_subclass = copy_class_recursive(subclass, target_ontology, source_ontology, api_key, custom_function)
-        target_subclass.is_a.append(target_class)
-
+        # target_subclass = copy_class_recursive(subclass, target_ontology, source_ontology, api_key, custom_function)
+        # target_subclass.is_a.append(target_class)
+        pass
     return target_class
 
 
@@ -72,7 +78,7 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Mat2DevPlatform.settings")
     api_key = settings.OPENAI_API_KEY
     onto_path.append("/home/mdreger/Documents/MatGraphAI/Ontology/")
-    onto = get_ontology("manufacturing.owl").load()
+    onto = get_ontology("manufacturing1.owl").load()
 
     starting_class = onto["Process"]
     print(list(onto.classes()))
